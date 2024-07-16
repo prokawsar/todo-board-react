a29bbdd41a309ee77fcbf60207288c32010bca36
prokawsar <prokawsar@gmail.com>
2024-07-16 18:59:44 +0600

    index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NextLogo from "./components/NextLogo";
+import SupabaseLogo from "./components/SupabaseLogo";
+import { supabase } from "./db/supabase";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-full flex-1 flex-col items-center gap-20">
+      <div className="animate-in flex max-w-4xl flex-1 flex-col px-3">
+        <main className="flex flex-1 flex-col items-center justify-center gap-6">
+          <h1 className="text-center text-3xl">
+            <span className="font-bold text-slate-600">Todo Board</span> built
+            with
+          </h1>
+          <div className="flex flex-row items-center  gap-3">
+            <NextLogo />
+            <SupabaseLogo />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
