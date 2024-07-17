9bc2877e5c32add79541bff5a439b496221fa46b
prokawsar <prokawsar@gmail.com>
2024-07-17 13:08:04 +0600

    Fixed visibility and disable state

--- src/pages/dashboard/card-details.tsx
+++ src/pages/dashboard/card-details.tsx
@@ -100,7 +100,7 @@ export default function CardDetails({ data, setShowDrawer }: Props) {
   return (
     data && (
       <div
-        className={`fixed right-0 top-0 z-40 h-screen w-full border-l-[1.5px] border-gray-200 bg-white pt-8 transition-all sm:translate-x-0 md:w-96`}
+        className={`fixed right-0 top-0 z-10 h-screen w-full border-l-[1.5px] border-gray-200 bg-white pt-8 transition-all sm:translate-x-0 md:w-96`}
         aria-label="sidebar"
       >
         <div className="relative h-full w-full overflow-y-auto bg-white px-3  pb-4">
