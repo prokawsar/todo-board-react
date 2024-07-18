c6ae26afd3db59bfcff96e6d7170b7b876b4f16a
prokawsar <prokawsar@gmail.com>
2024-07-18 20:09:06 +0600

    Fixed spacing

--- src/pages/dashboard/page.tsx
+++ src/pages/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex w-full max-w-7xl flex-1 flex-col items-center">
-      <div className="w-full bg-gradient-to-r from-transparent via-purple-700 to-transparent p-[1px]" />
+      <div className="w-full bg-gradient-to-r from-transparent via-purple-700 to-transparent p-[1.5px] mb-2" />
 
       <div
         className={`grid h-[85vh] w-full auto-cols-[16rem] grid-flow-col gap-x-2 overflow-x-auto`}
