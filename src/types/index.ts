d12c93303bbd6bd1e5d790bd75f0abb744886a11
Kawsar Ahmed <prokawsar@gmail.com>
2024-10-01 18:40:26 +0600

    Fix validation in zod

--- src/types/index.ts
+++ src/types/index.ts
@@ -25,9 +25,13 @@ export type History = {
 };
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Email is invalid" }),
   password: z
-    .string({ message: "Password is required" })
+    .string()
+    .min(1, { message: "Password is required" })
     .max(15, { message: "Password maximum length is 15" }),
 });
 
@@ -35,16 +39,18 @@ export type LoginFields = z.infer<typeof loginSchema>;
 
 export const signupSchema = z
   .object({
-    email: z.string().email(),
+    email: z.string().min(1, { message: "Email is required" }).email(),
     password: z
-      .string({ message: "Password is required" })
+      .string()
       .min(8, { message: "Password must contain at least 8 characters" })
       .max(15, { message: "Password maximum length is 15" })
       .regex(
         /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
         "Password must contain mix of digits, letters and any special characters"
       ),
-    confirm_password: z.string({ message: "Confirm password is required" }),
+    confirm_password: z
+      .string()
+      .min(1, { message: "Confirm password is required" }),
   })
   .refine((data) => data.password === data.confirm_password, {
     message: "Confirm password do not match",
